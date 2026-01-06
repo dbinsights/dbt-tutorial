30c6a3c85003c5097366924522887bba8f507854
Jason Liu <jason.liu@serko.com>
2026-01-06 14:52:09 +1300

    introduce jinja

--- jaffle_shop/models/mart/core/intermediate/order_payments.sql
+++ jaffle_shop/models/mart/core/intermediate/order_payments.sql
@@ -15,6 +15,14 @@ final as (
         sum(case when payment_method = '{{payment_method}}' then amount else 0 end) as {{payment_method}}_amount,
         {% endfor -%}
 
+        /*
+        https://docs.getdbt.com/guides/using-jinja?step=1
+        sum(case when payment_method = 'bank_transfer' then amount end) as bank_transfer_amount,
+        sum(case when payment_method = 'credit_card' then amount end) as credit_card_amount,
+        sum(case when payment_method = 'gift_card' then amount end) as gift_card_amount,
+        sum(case when payment_method = 'coupon' then amount end) as coupon_amount,
+        */
+
         sum(amount) as total_amount
 
     from payments
